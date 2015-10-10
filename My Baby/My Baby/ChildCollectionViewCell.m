9b29eebb6b94af1022f5a42a8ca4954f3ff79b87
Jason Wang <qwang216@gmail.com>
2015-10-10 16:54:32 -0400

    custom collection view cell

---
+++ My Baby/My Baby/ChildCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ChildCollectionViewCell.m
+//  My Baby
+//
+//  Created by Jason Wang on 10/10/15.
+//  Copyright © 2015 Henna. All rights reserved.
+//
+
+#import "ChildCollectionViewCell.h"
+
+@implementation ChildCollectionViewCell
+
+@end
