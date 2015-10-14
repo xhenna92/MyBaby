aae50e131da495965c1594a71d4cde703db0357a
Henna <henna.ahmed92@gmail.com>
2015-10-13 20:59:00 -0400

    added picker view for creating a child height

--- My Baby/My Baby/ChildAddViewController.m
+++ My Baby/My Baby/ChildAddViewController.m
@@ -9,11 +9,19 @@
 #import "ChildAddViewController.h"
 #import "Child.h"
 
-@interface ChildAddViewController ()
+@interface ChildAddViewController () <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (nonatomic) NSMutableArray * feetOptions;
+@property (nonatomic) NSMutableArray * inchesOptions;
 @property (nonatomic) BOOL gender;
 @property (strong, nonatomic) IBOutlet UIDatePicker *childDOBDatePicker;
 @property (nonatomic) NSString *childDOBString;
+@property (weak, nonatomic) IBOutlet UIPickerView *heightPickerView;
+@property (weak, nonatomic) IBOutlet UITextField *heightTextField;
+@property (weak, nonatomic) IBOutlet UITextField *weightTextField;
+@property (nonatomic) NSNumber *heightFt;
+@property (nonatomic) NSNumber *heightIn;
+
 
 @end
 
@@ -21,9 +29,91 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.feetOptions = [[NSMutableArray alloc] init];
+    self.inchesOptions = [[NSMutableArray alloc] init];
+    self.heightIn = 0;
+    self.heightFt = 0;
+    self.heightTextField.delegate = self;
+    self.heightPickerView.hidden = true;
     self.childDOBDatePicker.datePickerMode = UIDatePickerModeDate;
+    
+
+    
+    
+    UIToolbar *toolBar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, self.heightPickerView.frame.size.width, 44)];
+    toolBar.barStyle = UIBarStyleDefault;
+    UIBarButtonItem *btn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneBtnPressToGetValue)];
+    [toolBar setItems:[NSArray arrayWithObject:btn]];
+    [self.heightPickerView addSubview:toolBar];    
+    
+    
+    self.heightPickerView.delegate = self;
+    self.heightPickerView.dataSource=self;
+    self.heightPickerView.showsSelectionIndicator = YES;
 }
 
+- (void) doneBtnPressToGetValue {
+ self.heightPickerView.hidden = true;
+}
+
+#pragma mark - Height Picker View
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    // components are like columns. one column for feet and one for inches
+    return 2;
+}
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if(component == 0){
+        return [[self getUserDetailsHeightFeetOptions]count];
+    } else {
+        return [[self getUserDetailsHeightInchesOptions]count];
+    }
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if(component == 0){
+        return [NSString stringWithFormat:@"%@ ft", [[self getUserDetailsHeightFeetOptions]objectAtIndex:row]];
+    } else {
+        return [NSString stringWithFormat:@"%@ in", [[self getUserDetailsHeightInchesOptions]objectAtIndex:row]];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    if(component == 0)
+    {
+        self.heightFt = self.feetOptions[row];
+    }
+    else{
+        self.heightIn = self.inchesOptions[row];
+    }
+    
+
+    
+    
+}
+
+- (NSArray*)getUserDetailsHeightFeetOptions{
+    
+    for (int i = 3; i < 8; i++) {
+        [self.feetOptions addObject:[NSNumber numberWithInt:i]];
+    }
+    return self.feetOptions;
+}
+
+- (NSArray*)getUserDetailsHeightInchesOptions{
+
+    for (int i = 0; i < 12; i++) {
+        [self.inchesOptions addObject:[NSNumber numberWithInt:i]];
+    }
+    return self.inchesOptions;
+}
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+
+    [self.view endEditing:YES];
+    self.heightPickerView.hidden = false;
+    return false;
+    
+}
 
 #pragma mark - IBAction Interaction
 
